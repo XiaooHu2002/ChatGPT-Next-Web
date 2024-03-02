999849e0551983fb1ceba3bfd9d2da6336a8825f
严千屹 <90300662+XiaooHu2002@users.noreply.github.com>
2024-03-02 23:04:24 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -156,7 +156,9 @@ export function SideBar(props: { className?: string }) {
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
   ChatGPT-千屹镜像站<br />
-  返回-<a href="https://blog.qianyios.top" className={styles["custom-link"]}>严千屹博客</a>
+  返回主页—><span style="font-size: 12px">返回主页—>
+            <a href="https://blog.qianyios.top" className={styles["custom-link"]}>严千屹博客</a>
+            <—</span>
 </div>
         <div className={styles["sidebar-sub-title"]}>
           Build your own AI assistant.
