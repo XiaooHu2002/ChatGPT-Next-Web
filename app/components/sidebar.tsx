2ca3b3379d733809c9340a13c4e1f605cf69054d
严千屹 <90300662+XiaooHu2002@users.noreply.github.com>
2024-03-02 21:37:13 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,7 +155,7 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          NextChat
+          ChatGPT-千屹镜像站-<a href="https://blog.qianyios.top" style="text-decoration: none;">严千屹博客</a>
         </div>
         <div className={styles["sidebar-sub-title"]}>
           Build your own AI assistant.
